da6d9ee68588fdc32d43bfecdc08e99e02ae3273
zairyo huang <zairyo240@gmail.com>
2013-07-14 13:25:52 +0800

    version 1.2

---
+++ handler.js
@@ -0,0 +1,37 @@
+var fs=require('fs'),
+	mime=require('./mime');
+
+function Error(res){
+	res.writeHead(404);
+	res.end('404 Not Found');
+};
+
+function gethead(req,res,Ext,callback){
+	var head={};
+	fs.stat(req.path,function(err,stat){
+		if(err){return Error(res)}
+		head.sizes=stat.size;
+		head.mime=mime.type(Ext);
+		callback(head)
+	})
+};
+
+function handle(req,res,Ext,type){
+	if (type==0){
+		req.path=req.session.name?req.path:'./logn.html';
+	}
+	gethead(req,res,Ext,function(head){
+		fs.readFile(req.path,function(err,data){
+			if(err){return Error(err)}
+			res.writeHead(200,{'Content-Type':head.mime,
+							   'Content-Length':head.sizes,
+							   'Set-Cookie':'sid='+req.session.sid
+							   }	
+						);
+			res.write(data,'utf8');
+			res.end();			
+		})
+	})
+};
+
+exports.handle=handle;
--- router.js
+++ router.js
@@ -1,91 +1,30 @@
-var fs=require('fs');
-	events=require('events');
-	mime=require('./mime');
-	querystring=require('querystring');
-	session=require('./session');
-
-function Error(res){
-	res.writeHead(404);
-	res.end('404 Not Found');
-};
+var fs=require('fs'),
+	querystring=require('querystring'),
+	url=require('url'),
+	session=require('./session'),
+	handler=require('./handler');
 
 function route(req,res,path){
 	
 	var pathName={
-		'/':__dirname+path+'logn.html',
-		'/chatroom.html':__dirname+path,
-		'/logn.html':__dirname+path
+		'/':'./logn.html',
+		'/chatroom.html':'.'+path,
+		'/logn.html':'.'+path
 		},
-		event=new events.EventEmitter();
-		Ext='';		
-		
-	function count(obj){
-		var c=0;
-		for (i in obj){
-			if (obj.hasOwnProperty(i)){c++}
-		};
-		return c;
-	};
-	
-	if (pathName[path]){
-		fs.readFile(pathName[path],function(err,data){
-			console.log(pathName[path]);
-			if(err){return Error(res);};
-			
-			var cookies=querystring.parse(req.headers.cookie,';');
-			if (count(cookies)>0){
-				var s=cookies['sid']?cookies.sid:undefined;
+		Ext=path.slice(path.indexOf('.'),path.length-1),
+		cookies=querystring.parse(req.headers.cookie,';');
 
-				console.log(s);
-
-				req.cookie=cookies;
-				req.session=session.judge(s)?session.get(s):session.create(s);
-				res.setHeader('Set-Cookie','sid='+req.session.sid);
-
-				if(req.session.name&&(path=='/logn.html'||path=='/')){
-					fs.readFile(__dirname+'/chatroom.html',function(err,datas){
-						if (err){return Error(res)};
-						res.writeHead(200,{'Content-Type':'text/html'});
-						res.write(datas,'utf8');
-						res.end();
-					})
-					return;
-				}
-
-			}
-			else{
-				console.log('no cookie');
-				req.session=session.create()
-				res.setHeader('Set-cookie','sid='+req.session.sid);
-			};
-			res.writeHead(200,{'Content-Type':'text/html'});
-			res.write(data,'utf8');
-			res.end();
-		});
-	}
-	else{
-		event.on('sizegot',function(stat){
-			fs.readFile(__dirname+path,function(err,data){
-				if(err){return Error(res);};
-				res.writeHead(200,{
-							"content-type":mime.type(Ext),
-							"content-length":stat.size
-							});
-				res.end(data);
-			});
-		});
-		
-		Ext=path.slice(path.indexOf('.'),path.length-1);
-		
-		fs.stat(__dirname+path,function(err,stat){
-			if(err){
-			res.writeHead(404);
-			res.end('File doesn\'t exist');
-			return;
-			};
-			event.emit('sizegot',stat);
-		});	
-	}
+		if (pathName[path]){
+			req.path=pathName[path]?pathName[path]:('.'+path);
+			req.cookie=cookies;
+			req.session=session.judge(cookies['sid'])?session.get(cookies['sid']):session.create(cookies['sid']);
+			handler.handle(req,res,Ext,0)}
+		else{
+			req.path='.'+path;
+			req.session={};
+			req.session.sid='';
+			handler.handle(req,res,Ext,1);
+		}
 };
 
 exports.route=route;
